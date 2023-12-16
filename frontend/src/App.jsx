08cfa226037be9506a24748b5f683e1e040e8a26
DRIZZTAR <tyson_jeremy@hotmail.com>
2023-12-16 11:31:08 -0700

    Pass props from PhotoList to PhotoLIstItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,15 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import PhotoList from './components/PhotoList';
+import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-import PhotoFavButton from 'components/PhotoFavButton';
-
-const sampleDataForPhotoListItems = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    username: "Jane Example",
-    profile: `${process.env.PUBLIC_URL}/profile-2.jpg`,
-  },
-  {
-    id: "3",
-    location: {
-      city: "Vancouver",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    username: "John Example",
-    profile: `${process.env.PUBLIC_URL}/profile-3.jpg`,
-  },
-];
 
 const App = () => {
   return (
     <div className="App">
-      {sampleDataForPhotoListItems.map(item => (
-        <PhotoListItem key={item.id} props={item}/>
-      ))}
+      {/*sampleDataForPhotoListItems.map(item => (<PhotoListItem key={item.id} props={item}/>))}
+      {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+      <PhotoList />
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
-const PhotoListItem = ({props}) => {
+const PhotoListItem = ({ id, urls, user, location }) => {
   return (
-    <div className="photo-list__item" key={props.id}>
-<div className="photo-list__image-container">
-  <img className="photo-list__image" src={props.imageSource} alt="bannerPic" />
-  <PhotoFavButton />
-</div>
+    <div className="photo-list__item" key={id}>
+      <div className="photo-list__image-container">
+        <img className="photo-list__image" src={urls.regular} alt="bannerPic" />
+        <PhotoFavButton />
+      </div>
       <div className="photo-list__user photo-list__user-details">
-        <img className="photo-list__user-profile photo-list__user-details" src={props.profile} alt="profilePic" />
+        <img className="photo-list__user-profile photo-list__user-details" src={user.profile} alt="profilePic" />
         <div className="photo-list__user-info">
-          <h1 className="photo-list__user-info">{props.username}</h1>
-          <h2 className="photo-list__user-info photo-list__user-location">{props.location.city}, {props.location.country}</h2>
+          <h1 className="photo-list__user-info">{user.username}</h1>
+          <h2 className="photo-list__user-info photo-list__user-location">{location.city}, {location.country}</h2>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default PhotoListItem;
