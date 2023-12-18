5efa1748d43a95e408b0ea468cbcd9852e09cc52
DRIZZTAR <tyson_jeremy@hotmail.com>
2023-12-18 13:26:16 -0700

    Add stylings based on feedback

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import '../styles/HomeRoute.scss';
+import '../styles/PhotoList.scss';
 import TopNavigation from '../components/TopNavigationBar';
 import PhotoList from '../components/PhotoList';
 
@@ -9,7 +10,7 @@ const HomeRoute = ({ photos, topics, onPhotoClick, userFavourite, setUserFavouri
   return (
     <div className="home-route">
       <TopNavigation topics={topics} userFavourite={userFavourite} />
-      <PhotoList 
+      <PhotoList
         photos={photos} 
         userFavourite={userFavourite} 
         setUserFavourite={setUserFavourite} 
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,20 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoList.scss';
+import '../styles/PhotoListItem.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from '../components/PhotoList';
+import PhotoFavButton from '../components/PhotoFavButton';
 
 const PhotoDetailsModal = ({ onClose, photos, selectedPhoto, userFavourite, setUserFavourite }) => {
   const { id, urls, user, location } = selectedPhoto;
-  
 
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onClose}>
-        <img src={closeSymbol} alt="close symbol" />
+        <img className="photo-details-modal__close-button" src={closeSymbol} alt="close symbol" />
       </button>
-      <img className='photo-details-modal__image ' src={urls.full} alt="bannerPic" />
-      <PhotoList photos={photos} userFavourite={userFavourite} setUserFavourite={setUserFavourite} />
+      <div className="photo-list__item" key={id} style={{border: 'none'}}>
+        <div className="photo-list__image-container">
+          <img className="photo-details-modal__image" src={urls.regular} alt="bannerPic"/>
+          <PhotoFavButton className="photo-list__fav-icon" id={id} userFavourite={userFavourite} setUserFavourite={setUserFavourite}/>
+        </div>
+        <div className="photo-details-modal__info-header-container">
+          <div className="photo-details-modal__photographer-details">
+            <img className="photo-details-modal__photographer-profile" src={user.profile} alt="profilePic" />
+            <div className="photo-details-modal__photographer-info-container">
+              <div className="photo-details-modal__photographer-info">{user.username}</div>
+              <div className="photo-details-modal__photographer-info photo-details-modal__photographer-location">{location.city}, {location.country}</div>
+            </div>
+          </div>
+        </div>
+        <h2 className="photo-details-modal__header">Similar Photos</h2>
+      </div>
+      <PhotoList className="photo-details-modal__images" photos={photos} userFavourite={userFavourite} setUserFavourite={setUserFavourite} />
     </div>
   )
 };
