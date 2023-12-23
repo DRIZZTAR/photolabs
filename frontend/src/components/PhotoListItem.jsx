7dcd762cd1f69526d9f3863d8fdd0bd57007d193
DRIZZTAR <tyson_jeremy@hotmail.com>
2023-12-23 12:11:38 -0700

    reformatting propertys in PhotoListItem so elements with lots of properties are multi-line

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,14 +7,34 @@ const PhotoListItem = ({ id, urls, user, location, userFavourite, setUserFavouri
   return (
     <div className="photo-list__item" key={id}>
       <div className="photo-list__image-container">
-        <img className="photo-list__image" src={urls.regular} alt="bannerPic" onClick={() => onPhotoClick(photo)}/>
-        <PhotoFavButton className="photo-list__fav-icon" id ={ id } userFavourite={ userFavourite } setUserFavourite={ setUserFavourite } photo={photo}/>
+        <img 
+        className="photo-list__image" 
+        src={urls.regular} 
+        alt="bannerPic" 
+        onClick={() => onPhotoClick(photo)}
+        />
+        <PhotoFavButton 
+        className="photo-list__fav-icon"
+        id ={ id }
+        userFavourite={ userFavourite }
+        setUserFavourite={ setUserFavourite }
+        photo={photo}
+        />
       </div>
       <div className="photo-list__user photo-list__user-details">
-        <img className="photo-list__user-profile photo-list__user-details" src={user.profile} alt="profilePic" />
+        <img 
+        className="photo-list__user-profile photo-list__user-details"
+        src={user.profile}
+        alt="profilePic"
+        />
         <div className="photo-list__user-info">
-          <h1 className="photo-list__user-info">{user.username}</h1>
-          <h2 className="photo-list__user-info photo-list__user-location">{location.city}, {location.country}</h2>
+          <h2 className="photo-list__user-info">
+            {user.username}
+          </h2>
+          <h3 className="photo-list__user-info photo-list__user-location">
+            {location.city}, 
+            {location.country}
+          </h3>
         </div>
       </div>
     </div>
