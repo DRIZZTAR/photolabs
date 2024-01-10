fecd0a41704005486dccb6fec9ed6bb82a155b0e
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-01-10 12:19:29 -0700

    Fix bug in for favourite button moving outside of container in modal when changing window size

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
+/**
+ * PhotoFavButton Component
+ * Renders a button that allows users to mark or unmark a photo as a favorite.
+ *
+ * @param {Object} props - Component props
+ * @param {Object} props.photo - The photo object associated with this favorite button
+ * @param {Array} props.userFavourite - Array of user's favorite photos
+ * @param {Function} props.setUserFavourite - Function to update the user's favorite photos
+ */
+
 function PhotoFavButton({ photo, userFavourite, setUserFavourite }) {
 
   const isFavorited = userFavourite.some(favPhoto => favPhoto.id === photo.id);
@@ -16,6 +26,7 @@ function PhotoFavButton({ photo, userFavourite, setUserFavourite }) {
 
   return (
     <button className="photo-list__fav-icon" onClick={handleClick}>
+      {/* Render favorite icon with the appropriate state */}
       <FavIcon className="photo-list__fav-icon-svg" isFavorited={isFavorited} />
     </button>
   );
--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
+/**
+ * PhotoList Component
+ * Renders a list of photo items. Each item in the list is represented by a PhotoListItem component.
+ *
+ * @param {Object} props - Component props
+ * @param {Array} props.userFavourite - Array of user's favorite photos
+ * @param {Function} props.setUserFavourite - Function to update the user's favorite photos
+ * @param {Function} props.onPhotoClick - Function to handle the click event on a photo
+ * @param {Array} props.photos - Array of photo objects to be displayed in the list
+ */
 const PhotoList = ({ userFavourite, setUserFavourite, onPhotoClick, photos }) => {
   return (
     <div className="photo-list">
+      {/* Mapping through each photo and rendering a list item */}
       {photos && photos.map(photo => (
         <PhotoListItem 
           key={photo.id} 
